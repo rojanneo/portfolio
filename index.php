f4db28a25a50169ca7af7cee5e84094c1de0fe26
rojanneo <rojan_neo@hotmail.com>
2014-06-17 20:53:01 +0545

    first commit

---
+++ index.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<!--comment-->
+<meta name="viewport" content="width=device-width, maximum-scale = 1.0, minimum-scale = 1.0, initial-scale=1.0, user-scalable=no"/>
+<link rel = "stylesheet" type = "text/css"href="css/reset.css"/>
+<link rel = "stylesheet" type = "text/css"href="css/flexslider.css"/>
+<link rel = "stylesheet" type = "text/css" href="css/style.css"/>
+<link rel = "stylesheet" type = "text/css" href = "css/style_layout_large.css"/>
+<link rel = "stylesheet" type = "text/css" media = "only screen and (min-width:50px) and (max-width:500px)"href = "css/style_layout_small.css"/>
+<link rel = "stylesheet" type = "text/css" media = "only screen and (min-width:501px) and (max-width:800px)"href = "css/style_layout_medium.css"/>
+
+<!-- [If lt IE 9]
+<script type="text/javascript" src="js/html5shiv.js"></script>
+-->
+
+<script type="text/javascript" src="js/jquery-1.11.1.js"></script>
+<script type="text/javascript" src="js/jquery.flexslider.js"></script>
+<script type="text/javascript" src="js/site.js"></script>
+<title>Rojan Shrestha</title>
+</head>
+
+<body>
+<div class="page">
+	<div class="header">
+		<div class="top-nav">
+			<div class="logo">
+				<h1>Rojan Shrestha</h1>
+			</div>
+			<div class="top-menu">
+				<ul class="menu">
+					<li><a href="#">Home</a></li>
+					<li><a href="#">About</a></li>
+					<li><a href="#">Portfolio</a></li>
+					<li><a href="#">Resume</a></li>
+					<li><a href="#">Blog</a></li>
+				</ul>
+			</div>
+		</div>
+		<div class="tagline">
+			<ul class="slides">
+				<li>
+					<div class="slide-text">
+					<div class="text">
+						<h2>Heading Tag Line</h2>
+						<p>This is Line 1</p>
+						<p>and line 2</p>
+						<p>And this is line 3</p>
+					</div>
+						<a href="#">See Resume</a>					
+					</div>
+				</li>
+				<li>
+					<div class="slide-text">
+					<div class="text">
+						<h2>Heading Tag Line</h2>
+						<p>This is Line 1 also this is a very long line just to see what happens</p>
+						<p>and line 2</p>
+					</div>
+						<a href="#">See Resume</a>					
+					</div>
+				</li>
+				<li>
+					<div class="slide-text">
+					<div class="text">
+						<h2>Heading Tag Line</h2>
+						<p>This is Line 1</p>
+						<p>and line 2</p>
+						<p>And this is line 3</p>
+						<p>Along with line 4. this one is long</p>
+					</div>
+						<a href="#">See Resume</a>
+					</div>
+				</li>
+
+			</ul>
+		</div>
+	</div>
+	
+	<div class="highlights">
+		<div class="header-text">
+			<p>Highlighted Projects</p>
+			<a href="#">See Portfolio</a>
+		</div>
+		<div class="highlight-list">
+		<div class="highlight-1">
+			<a href="#"><img src="img/game_screen.png"/></a>
+		</div>
+		<div class="highlight-2">
+			<a href="#"><img src="img/game_screen1.png"/></a>
+		</div>
+		</div>
+	</div>
+	
+	<div class="social">
+		<div class="header-text">
+			<p>Find Me On:</p>
+		</div>
+		<ul class="social-links">
+			<li class="facebook"><a href="#">Facebook</a></li>
+			<li class="twitter"><a href="#">Twitter</a></li>
+			<li class="linkedin"><a href="#">Linked In</a></li>
+			<li class="behance"><a href="#">Behance</a></li>
+		</ul>
+	</div>
+	
+	<div class="contact-me">
+		<div class="contact-form">
+			<div class="header-text">
+				<p>Send Me a Message:</p>
+			</div>
+			<div class="form">
+			<form method="post" action="">
+				<input type="text" placeholder="Your Name"/>
+				<input type="email" placeholder="Email Address"/>
+				<input type="textbox" placeholder="Message"/>
+				<input type="submit" value="Send Message"/>
+			</form>
+			</div>
+		</div>
+		
+		<div class="contact-text">
+		<p>Feel Free to reach out!</p>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer volutpat dui in massa vulputate, sed bibendum turpis mattis. Ut sodales arcu ac mollis suscipit. In lobortis </p>
+		</div>
+	</div>
+	
+	<div class="footer">
+		<div class="copyright">
+			<p>&copy; Rojan Shrestha</p>
+		</div>
+		<div class="footer-menu">
+			<ul class="fmenu">
+				<li><a href="#">Home</a></li>
+				<li><a href="#">Portfolio</a></li>
+				<li><a href="#">Resume</a></li>
+				<li><a href="#">Blog</a></li>
+			</ul>
+		</div>
+	</div>
+</div>
+
+</body>
+
+</html>
---
+++ js/site.js
@@ -0,0 +1,12 @@
+
+$(window).load(function() {
+    $(".tagline").flexslider({
+        animation: "slide",
+		controlNav: false,
+		directionNav: true,
+		pausePlay:false,
+		slideshowSpeed: 5000,
+		animationSpeed:1000,
+    });
+});
+
